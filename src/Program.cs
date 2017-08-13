bb0f61b953ee1ee91efb4ad0976ccda46c1b4d39
FantasticFiasco <mattias.kindborg@gmail.com>
2017-08-13 16:51:50 +0200

    chore: add console app

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Serilog;
+using Serilog.Sinks.Http.BatchFormatters;
+
+namespace ElasticStackExample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.DurableHttp(
+                    requestUri: "http://localhost:31311",
+                    batchFormatter: new ArrayBatchFormatter())
+                .CreateLogger()
+                .ForContext<Program>();
+
+            var currentUser = new User { FirstName = "John", Surname = "Doe" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                logger.Information(
+                    "Logging heartbeat from {@User} on {Computer}",
+                    currentUser,
+                    Environment.MachineName);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ src/User.cs
@@ -0,0 +1,9 @@
+namespace ElasticStackExample
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
