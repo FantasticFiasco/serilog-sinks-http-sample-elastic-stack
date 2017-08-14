5fdcff24f45ca8eefc655b31fed62ad6ccc5b002
FantasticFiasco <mattias.kindborg@gmail.com>
2017-08-14 17:07:45 +0200

    wip1

---
+++ start-serilog.ps1
@@ -0,0 +1,18 @@
+function Print ([string] $Message) {
+    Write-Host $Message -ForegroundColor Blue
+}
+
+function Initialize-Image
+{
+    Print("Build Docker image")
+    docker build -t elastic-stack-sample ./src
+}
+
+function Start-Container
+{
+    Print("Start Docker container")
+    docker run -it --rm elastic-stack-sample
+}
+
+Initialize-Image
+Start-Container
