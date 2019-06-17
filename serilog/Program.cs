55091e5075ed87770411c858decc3f1a3a7e0ab2
Christian Crowhurst <ccrowhurst@ramplc.com>
2019-06-17 12:01:54 +0100

    feat(serilog): send log entries in elastic format

--- serilog/Program.cs
+++ serilog/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
 using Serilog;
-using Serilog.Formatting.Json;
+using Serilog.Formatting.Elasticsearch;
 using Serilog.Sinks.Http.BatchFormatters;
 using SerilogExample.Generators;
 
@@ -31,6 +31,11 @@ namespace SerilogExample
 
         private static LoggerConfiguration GetLogConfiguration()
         {
+
+            // note: use of ElasticsearchJsonFormatter is optional (but recommended as it produces 'idiomatic' json)
+            // If you don't want to take a dependency on 'Serilog.Formatting.Elasticsearch' package
+            // you can also other json formatters such as Serilog.Formatting.Json.Serilog.Formatting.Json.JsonFormatter
+
             bool.TryParse(Environment.GetEnvironmentVariable("USE_LOGSPOUT"), out var useLogspout);
             if (!useLogspout)
             {
@@ -38,6 +43,7 @@ namespace SerilogExample
                 return new LoggerConfiguration()
                     .WriteTo.DurableHttpUsingFileSizeRolledBuffers(
                         requestUri: "http://logstash:31311",
+                        textFormatter: new ElasticsearchJsonFormatter(),
                         batchFormatter: new ArrayBatchFormatter())
                     .WriteTo.Console();
             }
@@ -45,7 +51,7 @@ namespace SerilogExample
             {
                 // send logs only to stdout which will then be read by logspout
                 return new LoggerConfiguration()
-                    .WriteTo.Console(new JsonFormatter());
+                    .WriteTo.Console(new ElasticsearchJsonFormatter());
             }
         }
     }
