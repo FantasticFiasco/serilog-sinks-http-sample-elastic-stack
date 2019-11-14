860165db4a2b52d127bc8dcba66119f0455dca3b
FantasticFiasco <mattias.kindborg@gmail.com>
2019-11-14 19:54:30 +0100

    wip

--- build/update-elastic-stack-version.sh
+++ build/update-elastic-stack-version.sh
@@ -4,9 +4,12 @@ set -euxo pipefail
 README=./README.md
 DOCKERFILE_VERSION=`grep -oP '\d+\.\d+\.\d+' ./elastic-stack/elasticsearch/Dockerfile`
 
+# Update Windows instructions
 sed -i -E "s/\"kbn-version\", \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\"/\"kbn-version\", \"${DOCKERFILE_VERSION}\"/g" $README
+
+# Update Linux instructions
 sed -i -E "s/'kbn-version: [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'/\'kbn-version: ${DOCKERFILE_VERSION}\'/g" $README
 
-echo "$APPVEYOR_REPO_BRANCH"
+# Commit changes
 git add $README
-git status
+git diff-index --cached --quiet HEAD || git commit -m "docs(readme): update elastic version"
