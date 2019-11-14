221906350bb32d61b5b8140a4b1ce7cd0a6ea309
FantasticFiasco <mattias.kindborg@gmail.com>
2019-11-14 19:37:01 +0100

    wip

--- build/update-elastic-stack-version.sh
+++ build/update-elastic-stack-version.sh
@@ -2,8 +2,7 @@
 set -euxo pipefail
 
 README=./README.md
-#ELASTIC_VERSION=`grep -oP '\d+\.\d+\.\d+' ./elastic-stack/elasticsearch/Dockerfile`
-ELASTIC_VERSION=1.1.1
+ELASTIC_VERSION=`grep -oP '\d+\.\d+\.\d+' ./elastic-stack/elasticsearch/Dockerfile`
 
 sed -i -E "s/\"kbn-version\", \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\"/\"kbn-version\", \"${ELASTIC_VERSION}\"/g" $README
 sed -i -E "s/'kbn-version: [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'/\'kbn-version: ${ELASTIC_VERSION}\'/g" $README
