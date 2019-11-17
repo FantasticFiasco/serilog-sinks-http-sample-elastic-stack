f37019ab890d29eec1deb4b6aa75b2fa1ee92aca
FantasticFiasco <mattias.kindborg@gmail.com>
2019-11-17 09:30:33 +0100

    wip

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-pip --version
+pip install --user pipenv
 
 docker-compose -f elastic-stack/docker-compose.yml build
 docker-compose -f serilog/docker-compose.yml build
