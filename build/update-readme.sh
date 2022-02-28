ef6b3518386b51888d4e4221609eb8271d83ecfb
FantasticFiasco <mattias@kindb.org>
2022-03-01 00:05:09 +0100

    wip

--- build/update-readme.sh
+++ build/update-readme.sh
@@ -19,14 +19,21 @@ sudo chmod 600 ~/.ssh/id_rsa
 git remote set-url origin git@github.com:FantasticFiasco/serilog-sinks-http-sample-elastic-stack.git
 git config user.name FantasticFiasco
 git config user.email mattias@kindb.org
+
+echo "1"
 git checkout $APPVEYOR_REPO_BRANCH
+echo "2"
 
 # Update Elastic Stack version in README
 DOCKERFILE_ELASTIC_VERSION=`grep -oP '\d+\.\d+\.\d+' ./elastic-stack/elasticsearch/Dockerfile`
 sed -i -E "s/\"kbn-version\", \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\"/\"kbn-version\", \"${DOCKERFILE_ELASTIC_VERSION}\"/g" ./README.md
 sed -i -E "s/'kbn-version: [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'/\'kbn-version: ${DOCKERFILE_ELASTIC_VERSION}\'/g" ./README.md
+echo "3"
 git add ./README.md
+echo "4"
 git diff-index --cached --quiet HEAD || git commit -m "docs(readme): update elastic version"
+echo "5"
 
 # Push git changes
-# git push
+git push
+echo "6"
